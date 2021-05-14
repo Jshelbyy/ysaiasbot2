eaa6d9e19725de1711fc49d39b5cbea9303ec9a7
Jshelbyy <77311737+Jshelbyy@users.noreply.github.com>
2021-05-14 17:23:32 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,41 @@
+const help = (prefix) => {
+	return `🤬🔥BOT DE GUERRA🔥🤬
+	
+	*YSAIAS DOMINA 🔥💻*
+	
+🐊 Principais Comandos 🐊
+
+🤬${prefix}menu
+Função: mostra esse menu
+
+🤬${prefix}attp (texto)
+Função: converte texto em sticker piscante
+
+🤬${prefix}gerarcc
+Função: Gera um cartão de crédito
+
+🤬${prefix}criador
+Função: Contato do proprietário do bot
+
+🤬${prefix}listonline
+Função: Lista membros onlines no gp
+
+🤬${prefix}antilink (1 or 0)
+Função: bane todo links de grupo e canais do yt
+
+🤬${prefix}antilinkhard (1 or 0)
+Função: bane todo tipo de link
+
+🤬${prefix}antifake (1 or 0)
+Função: bane os número fake que entrar
+
+🤬${prefix}kick
+Função: bane o n° da msg marcada
+
+🤬${prefix}ban
+Função: bane o n° da msg respondida`
+}
+
+exports.help = help
+
+
