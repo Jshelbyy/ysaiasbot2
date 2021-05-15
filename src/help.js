3041ffaca74ba3725b2ffc74bc1b9547ef86a5c4
Jshelbyy <77311737+Jshelbyy@users.noreply.github.com>
2021-05-14 23:11:29 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,41 @@
+const help = (prefix) => {
+	return `😡 𝐁𝐎𝐓 𝐌𝐎𝐃𝐎 𝐆𝐔𝐄𝐑𝐑𝐀 😡
+
+*💻🔥YSAIAS DOMINA 🔥💻*
+
+🌻𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 𝗕𝗥𝗔𝗕𝗢𝗫🌻
+
+🌹፝⃟ ꙳${prefix}menu
+Função: mostra esse menu
+
+🌹፝⃟ ꙳${prefix}attp (texto)
+Função: converte texto em sticker piscante
+
+🌹፝⃟ ꙳${prefix}gerarcc
+Função: Gera um cartão de crédito
+
+🌹፝⃟ ꙳${prefix}criador
+Função: Contato do proprietário do bot
+
+🌹፝⃟ ꙳${prefix}listonline
+Função: Lista membros onlines no gp
+
+🌹፝⃟ ꙳${prefix}antilink (1 or 0)
+Função: bane todo links de grupo e canais do yt
+
+🌹፝⃟ ꙳${prefix}antilinkhard (1 or 0)
+Função: bane todo tipo de link
+
+🌹፝⃟ ꙳${prefix}antifake (1 or 0)
+Função: bane os número fake que entrar
+
+🌹፝⃟ ꙳${prefix}kick
+Função: bane o n° da msg marcada
+
+🌹፝⃟ ꙳${prefix}ban
+Função: bane o n° da msg respondida`
+}
+
+exports.help = help
+
+
